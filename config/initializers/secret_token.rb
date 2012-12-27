25e4e204559f0ce217c2e0262b60b0409a66178f
Debjyoti Majumder <debjyoti.majumder@gmail.com>
2012-12-28 00:13:20 +0530

    Created the project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArticleHouse::Application.config.secret_token = 'ced34da5a05193f9094062b5959ede965e6c9a0098d3dcc4f265d260c991e14ab6b1dbad71bc64a7c0ff53d670470acebd163f90db9b07d25024a967d1f76ecc'
