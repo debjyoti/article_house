7a7b4b1deda4055e69afe5f8ef9c0cfce9a25c9f
Debjyoti Majumder <debjyoti.majumder@gmail.com>
2012-12-31 00:06:52 +0530

    Another bug fix - update user role was not working.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,17 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to users_url }
+        format.json { head :no_content }
+      else
+        format.html { render action:"edit" }
+        format.json {render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
